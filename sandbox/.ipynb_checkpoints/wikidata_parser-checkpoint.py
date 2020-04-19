3a14e87a476cd8e3036f7cb6b63de5b45af22bbc
lubianat <tiago.lubiana.alves@usp.br>
2020-04-19 12:43:37 -0300

    Adding code to migrate protein-protein interactions from BioGrid to wikidata

---
+++ sandbox/.ipynb_checkpoints/wikidata_parser-checkpoint.py
@@ -0,0 +1,25 @@
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+def get_results(endpoint_url, query):
+    sparql = SPARQLWrapper(endpoint_url)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+def get_wikidata_items_for_property_value_pair(property_id, value ):
+    endpoint_url = "https://query.wikidata.org/sparql"
+    query = """SELECT ?item ?itemLabel
+    WHERE { 
+      ?item wdt:""" + property_id + '"' + str(value)  + '"' +   """
+    }
+    """
+    results = get_results(endpoint_url, query)
+    items  = list()
+    for result in results["results"]["bindings"]:
+        qid = result['item']['value']
+        items.append(qid.split('/')[4])
+
+    return(items)
+    
