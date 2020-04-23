b5d4b516b7afcc8473fbc0820548dfe8652788c9
jvfe <jvfecav@gmail.com>
2020-04-23 08:32:32 -0300

    QS API not working

--- sandbox/worldwide_data/src/qs_submit.sh
+++ sandbox/worldwide_data/src/qs_submit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+: '
+While the python script worked flawlessly, I still cant get 
+the QS API to work, but keep this as a placeholder if Its 
+able to work in the future.
+'
 date=$(printf '%(%Y-%m-%d)T\n' -1)
 
 curl https://tools.wmflabs.org/quickstatements/api.php \
