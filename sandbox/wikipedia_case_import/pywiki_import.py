254afcdc617e45ec7a9727ae380c132a89bcace6
lubianat <tiago.lubiana.alves@usp.br>
2021-01-08 23:35:59 -0300

    code: Prepare code for bot. Login issues to fix.

--- sandbox/wikipedia_case_import/pywiki_import.py
+++ sandbox/wikipedia_case_import/pywiki_import.py
@@ -7,16 +7,16 @@ repo = site.data_repository()
 item = pywikibot.ItemPage(repo, "Q102044164")
 item.get() #Fetch all page data, and cache it.
 
-for claim in item.claims['P527']: #Finds all statements (P131)
-    if 'P1120' in claim.qualifiers: #If has the qualifier we want to remove
-        for qual in claim.qualifiers['P1120']: #iterate over all P100
-            claim.removeQualifier(qual, summary=u'Remove qualifier.') #remove P100
-    if 'P1603' in claim.qualifiers: #If has the qualifier we want to remove 
-        for qual in claim.qualifiers['P1603']: #iterate over all P100
-            claim.removeQualifier(qual, summary=u'Remove qualifier.') #remove P100
-    if 'P1603' in claim.qualifiers: #If has the qualifier we want to remove
-        for qual in claim.qualifiers['P3457']: #iterate over all P100
-            claim.removeQualifier(qual, summary=u'Remove qualifier.') #remove P100
-    if 'P585' in claim.qualifiers: #If has the qualifier we want to remove
-        for qual in claim.qualifiers['P585']: #iterate over all P100
-            claim.removeQualifier(qual, summary=u'Remove qualifier.') #remove P100
+for claim in item.claims['P527']: 
+    if 'P1120' in claim.qualifiers:
+        for qual in claim.qualifiers['P1120']: 
+            claim.removeQualifier(qual, summary=u'Remove qualifier.') 
+    if 'P1603' in claim.qualifiers: 
+        for qual in claim.qualifiers['P1603']: 
+            claim.removeQualifier(qual, summary=u'Remove qualifier.') 
+    if 'P1603' in claim.qualifiers: 
+        for qual in claim.qualifiers['P3457']: 
+            claim.removeQualifier(qual, summary=u'Remove qualifier.') 
+    if 'P585' in claim.qualifiers: 
+        for qual in claim.qualifiers['P585']: 
+            claim.removeQualifier(qual, summary=u'Remove qualifier.')
